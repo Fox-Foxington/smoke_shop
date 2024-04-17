6018b00406f3549124d50f97635fad668f6e199d
Fox Foxington <fenfencutie@gmail.com>
2024-04-17 16:20:35 +0900

    fixed modal and shrunk carasole

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-import express from "express";
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables from .env file
-const app = express();
-const port = process.env.PORT;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+// Set the views directory
+app.set('views', join(__dirname, 'views'));
+
+// Route for the home page
+app.get('/', (req, res) => {
+    // Render the index.ejs template
+    res.render('index');
 });
 
-
-
+// Start the server
 app.listen(port, () => {
-    console.log(`Server ruinning on port ${port}.`);
-})
+    console.log(`Server running on port ${port}.`);
+});
